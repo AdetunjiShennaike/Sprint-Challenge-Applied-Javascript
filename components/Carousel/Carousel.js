6b499659390e0b894b82452c2151d9fdc8809893
Adetunji <ashennaike@gmail.com>
2019-03-08 12:46:19 -0500

    am still workingon the functionality, just saving at the point of a basic setup for the method and selectors

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,5 +1,25 @@
 class Carousel {
+    constructor(object) {
+    this.object = object;
+    
+    //grab left and right buttons
+    this.leftBtn = document.querySelector('.left-button');
+    this.rightBtn = document.querySelector('.right-button');
 
+    //grab all the images
+    this.imgs = document.querySelectorAll('.carousel img');
+
+    //
+    
+    }
+
+    moveLeft() {
+
+    }
+
+    moveRight() {
+
+    }
 }
 
 let carousel = document.querySelector();
@@ -9,6 +29,6 @@ let carousel = document.querySelector();
     2. You will need to grab a reference to all of the images
     3. Create a current index
     4. Those buttons are gonna need some click handlers.
-    5. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
+    5. Think of how you would animate this component. Make the cards slide in and out, or fade. It's up to you!
     6. Have fun!
 */
